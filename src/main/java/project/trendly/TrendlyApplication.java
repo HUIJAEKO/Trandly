6a5ba3abbf58a399cf0e5979ebeae8553e2cda13
HUIJAEKO <xrhgmlwox@naver.com>
2025-03-22 15:57:54 +0900

    first commit

---
+++ src/main/java/project/trendly/TrendlyApplication.java
@@ -0,0 +1,13 @@
+package project.trendly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrendlyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrendlyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/project/trendly/TrendlyApplicationTests.java
@@ -0,0 +1,13 @@
+package project.trendly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrendlyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
